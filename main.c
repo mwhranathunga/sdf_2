b13bf8cac648036c74797ff178fffb8c1e1f2329
CRashmika496 <CRashmika2006@gmail.com>
2026-06-01 09:44:20 +0530

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 
 int main()
 {
+    //Hello there!
+    
     int amount;// using int because withdrawal amounts are whole numbers without decimals
     int notes5000, notes1000;// using int because currency bills can only be counted as whole units
     int remaining;// using int because remaining is a whole number
